d143fa6d6fecee62b5e0737e91428f3cee0dcfbb
Stuart Childs <childss@codecavern.org>
2012-09-27 23:12:08 -0400

    Test cleanup.

--- test/specs/prefresh-test.js
+++ test/specs/prefresh-test.js
@@ -1,69 +1,50 @@
 define(['_', 'prefresh'], function(_, Prefresh) {
-    function Dummy() {
-        var self = this;
-
-        self.loaderFunc = function() {
-        };
-        self.dataHandler = function() {
-        };
-    }
-
-    function dummySpy(funcName, inst) {
-        var dummy = inst || new Dummy();
-
-        spyOn(dummy, funcName);
-
-        return dummy;
-    }
-
     describe("Prefresh Library", function() {
+        var loaderFunc, dataHandler;
+
         beforeEach(function() {
             jasmine.Ajax.useMock();
+
+            loaderFunc = jasmine.createSpy();
+            dataHandler = jasmine.createSpy();
         });
 
         it("should call a user-supplied function on init with prefetch data", function() {
-            var dummy = dummySpy('dataHandler');
             var prefetch = {
                 data: 'yes'
             };
 
-            var pf = new Prefresh(dummy.loaderFunc, dummy.dataHandler, prefetch);
+            var pf = new Prefresh(loaderFunc, dataHandler, prefetch);
 
             pf.init();
 
-            expect(dummy.dataHandler).toHaveBeenCalledWith(prefetch, true);
+            expect(dataHandler).toHaveBeenCalledWith(prefetch, true);
         });
 
         it("should call a user-supplied loader function with the user-supplied handler function on refresh", function() {
-            var dummy = dummySpy('dataHandler');
-            var loadedData = { data: 'usually' };
-            spyOn(dummy, 'loaderFunc').andReturn(loadedData);
-            var pf = new Prefresh(dummy.loaderFunc, dummy.dataHandler, {});
+            var pf = new Prefresh(loaderFunc, dataHandler, {});
 
             pf.init();
             pf.refresh();
 
-            expect(dummy.loaderFunc).toHaveBeenCalledWith(dummy.dataHandler, false);
+            expect(loaderFunc).toHaveBeenCalledWith(dataHandler, false);
         });
 
         it("should call a user-supplied handler function with a value of true on the first call, and a loader function on subsequent calls", function() {
-            var dummy = dummySpy('dataHandler');
-            spyOn(dummy, 'loaderFunc').andReturn({});
-            var pf = new Prefresh(dummy.loaderFunc, dummy.dataHandler, {});
+            var pf = new Prefresh(loaderFunc, dataHandler, {});
 
             pf.init();
-            expect(dummy.loaderFunc).not.toHaveBeenCalled();
-            expect(dummy.dataHandler).toHaveBeenCalledWith({}, true);
+            expect(loaderFunc).not.toHaveBeenCalled();
+            expect(dataHandler).toHaveBeenCalledWith({}, true);
 
             pf.refresh();
-            expect(dummy.loaderFunc).toHaveBeenCalledWith(dummy.dataHandler, false);
+            expect(loaderFunc).toHaveBeenCalledWith(dataHandler, false);
             pf.refresh();
-            expect(dummy.loaderFunc).toHaveBeenCalledWith(dummy.dataHandler, false);
+            expect(loaderFunc).toHaveBeenCalledWith(dataHandler, false);
         });
 
         it("init is an alias for refresh", function() {
-            var dummy = dummySpy('dataHandler');
-            var pf = new Prefresh(dummy.loaderFunc, dummy.dataHandler, {});
+            var pf = new Prefresh(loaderFunc, dataHandler, {});
             spyOn(pf, 'refresh');
 
             pf.init();
@@ -71,22 +52,19 @@ define(['_', 'prefresh'], function(_, Prefresh) {
         });
 
         it("should call the user-supplied load function to get the initial data if not given prefetch data", function() {
-            var dummy = dummySpy('dataHandler');
-            spyOn(dummy, 'loaderFunc').andReturn({});
-            var pf = new Prefresh(dummy.loaderFunc, dummy.dataHandler);
+            var pf = new Prefresh(loaderFunc, dataHandler);
 
             pf.init();
-            expect(dummy.loaderFunc).toHaveBeenCalledWith(dummy.dataHandler, true);
+            expect(loaderFunc).toHaveBeenCalledWith(dataHandler, true);
         });
 
         it("should make AJAX call if loader function returns an object", function() {
-            var dummy = dummySpy('dataHandler');
             var expectedAjax = {
                 url: '/some/url'
             };
 
-            spyOn(dummy, 'loaderFunc').andReturn(expectedAjax);
-            var pf = new Prefresh(dummy.loaderFunc, dummy.dataHandler);
+            loaderFunc.andReturn(expectedAjax);
+            var pf = new Prefresh(loaderFunc, dataHandler);
 
             pf.init();
 
@@ -94,11 +72,10 @@ define(['_', 'prefresh'], function(_, Prefresh) {
 
             mostRecentAjaxRequest().response({ response: 200, responseText: '{"data":1}'});
 
-            expect(dummy.dataHandler).toHaveBeenCalledWith({ data: 1 }, true);
+            expect(dataHandler).toHaveBeenCalledWith({ data: 1 }, true);
         });
 
         it("should call the success function returned by the loader if it exists", function() {
-            var dummy = dummySpy('dataHandler');
             var successCalled = false;
             var expectedAjax = {
                 url: '/some/url',
@@ -107,15 +84,15 @@ define(['_', 'prefresh'], function(_, Prefresh) {
                 }
             };
 
-            spyOn(dummy, 'loaderFunc').andReturn(expectedAjax);
-            var pf = new Prefresh(dummy.loaderFunc, dummy.dataHandler);
+            loaderFunc.andReturn(expectedAjax);
+            var pf = new Prefresh(loaderFunc, dataHandler);
 
             pf.init();
 
             mostRecentAjaxRequest().response({ response: 200, responseText: '{"data":1}'});
 
             expect(successCalled).toBeTruthy();
-            expect(dummy.dataHandler).toHaveBeenCalledWith({ data: 1 }, true);
+            expect(dataHandler).toHaveBeenCalledWith({ data: 1 }, true);
         });
     });
 });
